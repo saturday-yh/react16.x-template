55e8976f693c2443261904731b620d5cff4049ba
罗阳淏 <luoyanghao@leweikeji.com>
2020-03-25 15:45:52 +0800

    搭建react项目环境

---
+++ src/pages/home/index/index.js
@@ -0,0 +1,17 @@
+/*eslint-disable*/
+import React from 'react';
+import Css from '../../../assets/css/home/index/index.css';
+export default class  IndexComponent extends React.Component{
+    componentDidMount(){
+        console.log($(".app").html());
+    }
+    render(){
+        return(
+            <div class="app">
+                首页
+                
+                <div className={Css['header']}>头部</div>
+            </div>
+        );
+    }
+}
